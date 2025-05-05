332b5b27893f521162b98a041beb565eb13cd6ea
suriya-shanmugam <suriya.shanmugam.us@gmail.com>
2025-05-04 21:43:59 -0700

    code refactor

--- full_node_server.py
+++ full_node_server.py
@@ -17,7 +17,16 @@ from proto import block_chain_pb2_grpc
 from modules.block import Block 
 from modules.merkle import MerkleTree 
 
+
+
+
 BLOCK_SIZE = 3
+
+class AuditDetails ():
+    def __init__(self, audit_id , block_id):
+        self.audit_id = audit_id
+        self.block_id = block_id        
+
 
 class ServerProperties () :
     def __init__(self,args):
@@ -25,14 +34,32 @@ class ServerProperties () :
         self.port = args.port
         self.isvalidator = args.isvalidator
         self.blocks = [Block.create_genesis_block()]
-        self.audit_details_byfile = {}
+        self.audit_details_byfile = {}  # Lookup data strcture based on File Id
+    
+    def create_block(self, audits, merkle_tree):
+        
+        last_block = server_properties.blocks[-1]
+        new_block = Block(index = last_block.index+1,
+                          previous_hash=last_block.hash,
+                          audits=audits,
+                          merkle_root=merkle_tree.root)
+        
+        self.blocks.append(new_block)
+        return new_block
+    
+    def store_file_audits(self,file_id, audit_id, block_id):
+        
+        audit_details = AuditDetails(audit_id, block_id)
+        if file_id in server_properties.audit_details_byfile :
+            server_properties.audit_details_byfile[file_id].append(audit_details)
+        else :
+            server_properties.audit_details_byfile[file_id] = [audit_details]
+        
+        print(server_properties.audit_details_byfile[file_id]) 
+        
         
 
-class AuditDetails ():
-    def __init__(self, audit_id , block_id, block_index ):
-        self.audit_id = audit_id
-        self.block_id = block_id
-        self.block_index =  block_index       
+     
 
 class FileAuditService(file_audit_pb2_grpc.FileAuditServiceServicer):
 
@@ -52,7 +79,7 @@ class FileAuditService(file_audit_pb2_grpc.FileAuditServiceServicer):
     
     async def SubmitAudit(self, request, context):
         
-        print("Printing the request")
+        print("Got a new audit request")
         print(request)
         future = asyncio.Future()
         await server_properties.request_queue.put((request,future))
@@ -72,44 +99,39 @@ class FileAuditService(file_audit_pb2_grpc.FileAuditServiceServicer):
                 same_block_audits = []
                 request_and_future_list = []
                 
+                
                 for _ in range(BLOCK_SIZE):
                     req_and_future =  await server_properties.request_queue.get()
                     request_and_future_list.append(req_and_future)
-                    request = req_and_future[0]
-                    audit_info = str(request.audit_info)
+                    audit_request = req_and_future[0]
+                    audit_info = str(audit_request.audit_info)
                     same_block_audits.append(audit_info)
 
                 
+                # create merkle tree
                 merkle_tree = MerkleTree(same_block_audits)
-                last_block = server_properties.blocks[-1]
-                new_block = Block(index = last_block.index+1,
-                          previous_hash=last_block.hash,
-                         audits=same_block_audits,merkle_root=merkle_tree.root)
-        
-                server_properties.blocks.append(new_block)
+                
+                # create new block in the chain
+                new_block = self.server_properties.create_block(same_block_audits,merkle_tree)
+                
+               
                 
                 index = 0
                 for request,future  in request_and_future_list :
-                    
                     file_id = request.file_id
-                    audit_details = AuditDetails(request.audit_info.audit_id, new_block.hash, index)
-                    
-                    if file_id in server_properties.audit_details_byfile :
-                        server_properties.audit_details_byfile[file_id].append(audit_details)
-                    else :
-                        server_properties.audit_details_byfile[file_id] = [audit_details]
-                        
-                    print(server_properties.audit_details_byfile[file_id])    
-                    response = file_audit_pb2.FileAuditResponse(merkle_proof = merkle_tree.get_merkle_proof(index), 
-                                                                       merkle_root = merkle_tree.root,
-                                                                       audit_index = index,
-                                                                       status="success")
-                    
-                    print(response)
-                    
+                    audit_id = request.audit_info.audit_id
+                    block_hash = new_block.hash
+                     # persist the every audit's block information - 
+                    self.server_properties.store_file_audits(file_id,audit_id,block_hash)
+                    response = file_audit_pb2.FileAuditResponse(status="success",
+                                                                merkle_proof = merkle_tree.get_merkle_proof(index), 
+                                                                merkle_root = merkle_tree.root,
+                                                                audit_index = index)
                     future.set_result(response)
                     index +=1
 
+            
+            
             
             print("Checking the queue...")
             await asyncio.sleep(3)        
